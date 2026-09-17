64c9da67e6cc768ab841c7b264240c0c02a79c02
Ivan Arkhipov <vanarhipov2007@mail.ru>
2026-09-17 17:42:15 +0300

    fixed error in main.py and start.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,17 +3,19 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-from main import (
-    calculate_frequencies,
-    get_top_n_words,
-    remove_stop_words,
-    tokenize,
-)
+
 
 def main() -> None:
     """
     Launches an implementation.
     """
+from main import (
+    calculate_frequencies,
+    get_top_n_words,
+    remove_stop_words,
+    tokenize,
+    )
+
     with open("lab_1_classify_profile/assets/texts/de.txt", "r", encoding="utf-8") as file:
         de_text = file.read()
     with open("lab_1_classify_profile/assets/texts/unknown.txt", "r", encoding="utf-8") as file:
@@ -26,10 +28,14 @@ def main() -> None:
     tokens_without_stopwords = remove_stop_words(tokens, stopwords)
     freq_dict = calculate_frequencies(tokens_without_stopwords)
     result = get_top_n_words(freq_dict, 7)
+    print(result)
     assert result, "Detection result is None"
+
+
 
 if __name__ == "__main__":
     main()
+
 
 
 
